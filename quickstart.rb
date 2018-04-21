286153d1a8f19be405c2cc60db6b8101b0a4404d
Ian Harris <ian@fofgof.xyz>
2018-04-21 02:47:10 -0700

    init commit

---
+++ quickstart.rb
@@ -0,0 +1,19 @@
+require 'bundler'
+Bundler.require()
+Dotenv.load
+
+sl = ENV['secret'].length
+
+post '/door' do
+    Twilio::TwiML::VoiceResponse.new.gather(numDigits: sl, action: '/input') do |g|
+        g.say(ENV['message'])
+    end.to_s
+end
+
+post '/input' do
+    Twilio::TwiML::VoiceResponse.new do |resp|
+        if params['Digits'] == ENV['secret']
+            resp.play(digits: ENV['digit'])
+        end
+    end.to_s
+end
